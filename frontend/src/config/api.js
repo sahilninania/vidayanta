3a2e5af77cf4b9b1a7e264462ef944ee9f4edd6e
sahil <sahilninania54@email.com>
2026-04-13 21:33:08 +0530

    fix vite build for vercel

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,6 @@
-const API_URL= import.meta.env.VITE_API_URL||"http://localhost:5000";
+// const API_URL= import.meta.env.VITE_API_URL||"http://localhost:5000";
+// export default API_URL;
+
+const API_URL = "https://vidayanta-backend.onrender.com";
+
 export default API_URL;
