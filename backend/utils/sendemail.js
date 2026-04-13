dde5e2dcbbd2dea2345038b7148bf63e3a824540
sahil <sahilninania54@email.com>
2026-04-13 19:08:56 +0530

    fix email dd

--- backend/utils/sendemail.js
+++ backend/utils/sendemail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     await resend.emails.send({
-      from: "noreplyvidayanta@gmail.com",
+      from: "Vidayanta <noreplyvidayanta@gmail.com>",
       to,
       subject,
       html,
