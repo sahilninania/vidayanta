cc4cc62387b3ab25619b415408e2b1ddb2fdd91c
sahil <sahilninania54@email.com>
2026-04-14 12:16:24 +0530

    fix resend not defined

--- backend/utils/sendemail.js
+++ backend/utils/sendemail.js
@@ -1,3 +1,7 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const response = await resend.emails.send({
