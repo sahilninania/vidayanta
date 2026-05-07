d20cefb730c1c91d02fea88b678c2e4cc4f6f855
sahil <sahilninania54@email.com>
2026-05-07 16:20:45 +0530

    fix index.html final

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -130,17 +130,16 @@ const signInController = async (req, res) => {
         institutionCode: user.institutionCode,
       },
       process.env.JWT_SECRET_KEY,
-      { expiresIn: "7d" }
+      { expiresIn: "30d" }
     );
 
     // 🍪 cookie
-    res.cookie("token", token, {
+   res.cookie("token", token, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
-      // sameSite: "lax",
-       secure: false,       // local ke liye
-       sameSite: "lax",
-       path :"/"
+      secure: false,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     });
 
     // ✅ response
